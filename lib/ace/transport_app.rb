aed929fc59e8749ecc6357e7efd70d023daaca5e
Thomas-Franklin <thomas.franklin@puppet.com>
2019-03-07 15:33:51 +0000

    Testing using a local remote transport

--- lib/ace/transport_app.rb
+++ lib/ace/transport_app.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require 'ace'
-require 'ace/executor'
+require 'bolt'
 require 'bolt/target'
 require 'bolt/task'
+require 'bolt/inventory'
+require 'bolt/task/puppet_server'
+require 'bolt_server/file_cache'
 require 'json'
 require 'json-schema'
 require 'sinatra'
@@ -12,7 +15,6 @@ module ACE
   class TransportApp < Sinatra::Base
     def initialize(config = nil)
       @config = config
-      @executor = ACE::Executor.new('production')
 
       @schemas = {
         "run_task" => JSON.parse(File.read(File.join(__dir__, 'schemas', 'ace-run_task.json')))
@@ -20,6 +22,10 @@ module ACE
       shared_schema = JSON::Schema.new(JSON.parse(File.read(File.join(__dir__, 'schemas', 'task.json'))),
                                        Addressable::URI.parse("file:task"))
       JSON::Validator.add_schema(shared_schema)
+
+      @executor = Bolt::Executor.new(0, load_config: false)
+
+      @file_cache = BoltServer::FileCache.new(@config).setup
 
       super(nil)
     end
@@ -31,6 +37,13 @@ module ACE
                        'ace/schema-error',
                        schema_error)
       end
+    end
+
+    def scrub_stack_trace(result)
+      if result.dig(:result, '_error', 'details', 'stack_trace')
+        result[:result]['_error']['details'].reject! { |k| k == 'stack_trace' }
+      end
+      result
     end
 
     get "/" do
@@ -46,20 +59,21 @@ module ACE
       content_type :json
 
       body = JSON.parse(request.body.read)
-      error = validate_schema(@schemas["run_task"], body)
-      return [400, error.to_json] unless error.nil?
+      #error = validate_schema(@schemas["ace-run_task"], body)
+#      return [400, error.to_json] unless error.nil?
 
-      # grab the transport connection_info
-      connection_info = Hash[body['target'].map { |k, v| [k.to_sym, v] }] # may contain sensitive data
+      opts = body['target'].merge('protocol' => 'local')
 
-      # originally this was a Bolt::Task::PuppetServer; simplified here for hacking
-      task = Bolt::Task.new(body['task'])
+      target = [Bolt::Target.new(body['target']['hostname'], opts)]
+
+      task = Bolt::Task::PuppetServer.new(body['task'], @file_cache)
 
       parameters = body['parameters'] || {}
 
-      result = @executor.run_task(connection_info, task, parameters)
-
-      [result.first, result.last.to_json]
+      # Since this will only be on one node we can just return the first result
+      results = @executor.run_task(target, task, parameters)
+      result = scrub_stack_trace(results.first.status_hash)
+      [200, result.to_json]
     end
   end
 end
